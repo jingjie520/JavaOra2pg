3acee770d21dae684229d3cacb5e2820e04ed1c0
SnowFox <jingjie520@gmail.com>
2022-07-05 00:50:47 +0800

    Init SpringBoot

---
+++ src/main/java/net/ycot/ora2pg/Ora2pgApplication.java
@@ -0,0 +1,13 @@
+package net.ycot.ora2pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ora2pgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ora2pgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ycot/ora2pg/Ora2pgApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ycot.ora2pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ora2pgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
